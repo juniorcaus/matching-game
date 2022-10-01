7e8931d340baf15678573157e60d65cfef22bb25
juniorcaus <miltomjuniorcausgervasio@hotmail.com>
2022-10-01 20:03:33 -0300

    :tada: inicio projeto
    inicio do projeto

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,12 @@
+import './style.css'
+
+function CardGame() {
+    return   `
+
+        <article class="card-game" >
+           <img src="images/alura-pixel.png"  alt="logo">
+        </article>
+    `;
+}
+
+export default CardGame;
